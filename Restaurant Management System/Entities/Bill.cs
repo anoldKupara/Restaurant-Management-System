a8a010084576e3addee86377f10e9352bbe6bee3
Anold Kupara <kuparaanold20@gmail.com>
2023-01-31 15:18:43 +0200

    added the bill model class

---
+++ Restaurant Management System/Entities/Bill.cs
@@ -0,0 +1,14 @@
+﻿namespace Restaurant_Management_System.Entities
+{
+    public class Bill
+    {
+        public int BillId { get; set; }
+        public int OrderId { get; set; }
+        public decimal TotalAmount { get; set; }
+        public decimal Tax { get; set; }
+        public decimal Discount { get; set; }
+        public DateTime Date { get; set; }
+
+        public Order Order { get; set; }
+    }
+}
