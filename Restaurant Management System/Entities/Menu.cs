d481fbe1100299f31f29bf044901848794009787
Anold Kupara <kuparaanold20@gmail.com>
2023-01-31 14:43:12 +0200

    added the Menu model class

---
+++ Restaurant Management System/Entities/Menu.cs
@@ -0,0 +1,11 @@
+﻿namespace Restaurant_Management_System.Entities
+{
+    public class Menu
+    {
+        public int MenuId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public byte[] Image { get; set; }
+    }
+}
