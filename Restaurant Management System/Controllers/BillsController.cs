e2e2cd4fa07194f30379a8662b9d3462f4524725
Anold Kupara <kuparaanold20@gmail.com>
2023-02-01 16:24:46 +0200

    added BillsController

---
+++ Restaurant Management System/Controllers/BillsController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Restaurant_Management_System.DbContexts;
+using Restaurant_Management_System.Entities;
+
+namespace Restaurant_Management_System.Controllers
+{
+    public class BillsController : Controller
+    {
+        private readonly MyDbContext _context;
+
+        public BillsController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Bills
+        public async Task<IActionResult> Index()
+        {
+            var myDbContext = _context.Bills.Include(b => b.Order);
+            return View(await myDbContext.ToListAsync());
+        }
+
+        // GET: Bills/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Bills == null)
+            {
+                return NotFound();
+            }
+
+            var bill = await _context.Bills
+                .Include(b => b.Order)
+                .FirstOrDefaultAsync(m => m.BillId == id);
+            if (bill == null)
+            {
+                return NotFound();
+            }
+
+            return View(bill);
+        }
+
+        // GET: Bills/Create
+        public IActionResult Create()
+        {
+            ViewData["OrderId"] = new SelectList(_context.Orders, "OrderId", "OrderId");
+            return View();
+        }
+
+        // POST: Bills/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("BillId,OrderId,TotalAmount,Tax,Discount,Date")] Bill bill)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(bill);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["OrderId"] = new SelectList(_context.Orders, "OrderId", "OrderId", bill.OrderId);
+            return View(bill);
+        }
+
+        // GET: Bills/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Bills == null)
+            {
+                return NotFound();
+            }
+
+            var bill = await _context.Bills.FindAsync(id);
+            if (bill == null)
+            {
+                return NotFound();
+            }
+            ViewData["OrderId"] = new SelectList(_context.Orders, "OrderId", "OrderId", bill.OrderId);
+            return View(bill);
+        }
+
+        // POST: Bills/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("BillId,OrderId,TotalAmount,Tax,Discount,Date")] Bill bill)
+        {
+            if (id != bill.BillId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(bill);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BillExists(bill.BillId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["OrderId"] = new SelectList(_context.Orders, "OrderId", "OrderId", bill.OrderId);
+            return View(bill);
+        }
+
+        // GET: Bills/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Bills == null)
+            {
+                return NotFound();
+            }
+
+            var bill = await _context.Bills
+                .Include(b => b.Order)
+                .FirstOrDefaultAsync(m => m.BillId == id);
+            if (bill == null)
+            {
+                return NotFound();
+            }
+
+            return View(bill);
+        }
+
+        // POST: Bills/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Bills == null)
+            {
+                return Problem("Entity set 'MyDbContext.Bills'  is null.");
+            }
+            var bill = await _context.Bills.FindAsync(id);
+            if (bill != null)
+            {
+                _context.Bills.Remove(bill);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BillExists(int id)
+        {
+          return _context.Bills.Any(e => e.BillId == id);
+        }
+    }
+}
